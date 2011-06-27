eb0d45f7050a8425ab2733ee4b9c4fd7226c1ae1
Kwame Afram <afram.sg@gmail.com>
2011-06-27 11:07:11 +0000

    I just updated the MAIN urls.py file

--- codeForum/urls.py
+++ codeForum/urls.py
@@ -13,5 +13,7 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^forum/',include('codeForum.forum.urls'))
+    url(r'^reg/', include('codeForum.forum.urls'))
 )
