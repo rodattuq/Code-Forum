8d737008594f63babf9d81cde6057e56c163a6b4
Kwame Afram <afram.sg@gmail.com>
2011-06-27 11:51:14 +0000

    ok

---
+++ codeForum/forum/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('',
+#	url(r'^$', 'forum.views.home'),
+#	url(r'^list/(\d+)?$', 'blog.views.blog_list'),
+#	url(r'^(detail|info)/(?P<id>\d+)/((?P<showComments>.*)/)?$', 'blog.views.blog_detail'),
+#	url(r'^search/(.*)$', 'blog.views.blog_search'),
+#	url(r'^editcomment/(\d+)$','blog.views.edit_comment'),
+       # url(r'^login/$', 'reg.views.loginView'), #
+        #url(r'^login/$', 'reg.views.logoutView'), #
+	
+)
